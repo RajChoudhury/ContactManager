40d386c85315c2d290200d51f25ad732453f77ac
RajChoudhury <up620432@myport.ac.uk>
2015-01-22 12:58:08 +0000

    Initial commit

---
+++ app/src/main/java/com/example/rifaz/contactmanager/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.rifaz.contactmanager;
+
+import android.media.effect.Effect;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import java.lang.CharSequence;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    EditText txtName, txtPhone, txtEmail, txtAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtName = (EditText) findViewById(R.id.nameTxt);
+        txtPhone = (EditText) findViewById(R.id.phoneTxt);
+        txtEmail = (EditText) findViewById(R.id.emailTxt);
+        txtAddress = (EditText) findViewById(R.id.addressTxt);
+        final Button btnAdd = (Button) findViewById(R.id.addBtn);
+
+        txtName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            btnAdd.setEnabled(!txtName.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
